628111d853dedb0f24a50852647ada615b7c23c1
SMTamiM <tamimmahmud0@gmail.com>
2022-03-16 02:28:47 +0600

    "Some improvements"

--- assets/scripts/linear_search.js
+++ assets/scripts/linear_search.js
@@ -1,4 +1,4 @@
-import { sleep, showHead, hideHead } from "./common.js";
+import { sleep, showHead, hideHead, showTopHead, hideTopHead, hideAll } from "./common.js";
 /**
  * The implementation of Linear Search Algorithm 
  */
@@ -9,22 +9,25 @@ async function linear_search(x, verticalBars){
         let number = element.querySelector('.number');
         number = parseInt(number.innerText);
         // Hide previous items head
-        if (i!=0)
+        if (i!=0){
             hideHead(verticalBars[i-1])
+            hideTopHead(verticalBars[i-1])
+        }
         // Show head on current item
         showHead(element);
-        await sleep(200);
+        showTopHead(element)
+        await sleep(300);
         if(number === parseInt(x)){
             found = true;
             console.log("Found");
             alert(`Found at position ${i+1}`);
             // Hide head of current item
-            hideHead(element);
+            hideAll(verticalBars);
             break;
         }
     };
     if(!found){
-        verticalBars[verticalBars.length-1].querySelector('.display_head').classList.remove('show_display_head');
+        hideAll(verticalBars);
         alert(`${x} Was not found in the array! :(`);
     } 
 }
